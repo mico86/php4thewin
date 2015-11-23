8fb9ebd53739a40cfdf089b1974e63c5f3db4224
davideghz <davideghz@gmail.com>
2015-11-23 17:11:19 +0100

    add file dave.php

---
+++ dave.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<title>DB experiment</title>
+	
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js">
+	</script>
+	<![endif]-->
+	
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	
+	<body>
+		<div class="container">
+		
+		<form action="subscribe.php" method="post">
+			<p>
+				Name:<br />
+				<input type="text" id="name" name="name" size="20" maxlength="40" />
+			</p>
+			<p>
+				Email Address:<br />
+				<input type="text" id="email" name="email" size="20" maxlength="40" />
+			</p>
+			<input type="submit" id="submit" name = "submit" value="Go!" />
+		</form>
+				
+		<?php				
+			$servername = "localhost";
+			$username = "root";
+			$password = "kalimero";
+
+			// Create connection
+			$conn = new mysqli($servername, $username, $password);
+
+			// Check connection
+			if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+			} 
+			echo "Connected successfully";
+			
+			echo "<br>";
+			
+			$sql = "SELECT actor_id, first_name, last_name FROM sakila.actor";
+			$result = $conn->query($sql);
+
+			if ($result->num_rows > 0) {
+				// output data of each row
+				while($row = $result->fetch_assoc()) {
+					echo "id: " . $row["actor_id"]. " - Name: " . $row["first_name"]. " " . $row["last_name"]. "<br>";
+				}
+			} else {
+				echo "0 results";
+			}
+			$conn->close();
+		?>
+		</div>
+	</body>
+</html>
