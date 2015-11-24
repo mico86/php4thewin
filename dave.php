3c40da60af924d691571364b513fdf9843e10c5e
davideghz <davideghz@gmail.com>
2015-11-24 11:35:41 +0100

    update dave.php to show form input. Labels added to form.

--- dave.php
+++ dave.php
@@ -17,19 +17,29 @@
 	<body>
 		<div class="container">
 		
-		<form action="subscribe.php" method="post">
+		<form action="dave.php" method="post">
 			<p>
-				Name:<br />
+				<label for="name">Name:</label> <br />
 				<input type="text" id="name" name="name" size="20" maxlength="40" />
 			</p>
 			<p>
-				Email Address:<br />
+				<label for="email">email:</label> <br />
 				<input type="text" id="email" name="email" size="20" maxlength="40" />
 			</p>
 			<input type="submit" id="submit" name = "submit" value="Go!" />
 		</form>
 				
-		<?php				
+		<?php
+
+			// If the name field is filled in
+			if (isset($_POST['name']))
+			{
+				$name = $_POST['name'];
+				$email = $_POST['email'];
+				printf("Hi %s! <br />", $name);
+				printf("The address %s will soon be a spam-magnet! <br />", $email);
+			}
+
 			$servername = "localhost";
 			$username = "root";
 			$password = "kalimero";
