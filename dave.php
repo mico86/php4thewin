9cfc0991c15536faa343a3ef91092121931d4639
davideghz <davideghz@gmail.com>
2015-11-30 15:25:52 +0100

    add cards and navbar

--- dave.php
+++ dave.php
@@ -18,9 +18,58 @@
 	<body>
 		<div class="container">
 
+			<nav class="navbar navbar-inverse">
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Brand</a>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+							<li><a href="#">Link</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Action</a></li>
+									<li><a href="#">Another action</a></li>
+									<li><a href="#">Something else here</a></li>
+									<li role="separator" class="divider"></li>
+									<li><a href="#">Separated link</a></li>
+									<li role="separator" class="divider"></li>
+									<li><a href="#">One more separated link</a></li>
+								</ul>
+							</li>
+						</ul>
+
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#">Link</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Action</a></li>
+									<li><a href="#">Another action</a></li>
+									<li><a href="#">Something else here</a></li>
+									<li role="separator" class="divider"></li>
+									<li><a href="#">Separated link</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+
 			<div class="row">
 				<div class="col-md-4 col-sm-12">
-					<div class="thumbnail no-margin-bottom card-dark">
+					<div class="thumbnail card-dark">
 						<img src="cardbg.jpg" class="img-responsive">
 						<div class="caption">
 							<h3 id="thumbnail-label">Thumbnail label<a class="anchorjs-link" href="#thumbnail-label"><span class="anchorjs-icon"></span></a></h3>
@@ -30,7 +79,7 @@
 					</div>
 				</div>
 				<div class="col-md-4 col-sm-12">
-					<div class="thumbnail no-margin-bottom">
+					<div class="thumbnail">
 						<img src="cardbg.jpg" class="img-responsive">
 						<div class="caption">
 							<h3 id="thumbnail-label">Thumbnail label<a class="anchorjs-link" href="#thumbnail-label"><span class="anchorjs-icon"></span></a></h3>
@@ -41,7 +90,7 @@
 				</div>
 
 				<div class="col-md-4 col-sm-12">
-					<div class="thumbnail no-margin-bottom card-dark">
+					<div class="thumbnail card-dark">
 						<img src="cardbg.jpg" class="img-responsive">
 						<div class="caption">
 							<h3 id="thumbnail-label">Thumbnail label<a class="anchorjs-link" href="#thumbnail-label"><span class="anchorjs-icon"></span></a></h3>
@@ -52,7 +101,7 @@
 				</div>
 			</div>
 
-
+			<div class=""row">
 				<div class="formbox">
 					<form class="form-inline" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
 						<div class="form-group">
@@ -68,6 +117,7 @@
 						<button type="submit" id="submit" name = "submit" class="btn btn-default">Add person</button>
 					</form>
 				</div>
+			</div>
 
 
 		<?php
